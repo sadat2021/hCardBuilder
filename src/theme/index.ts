70a1e2e0f61ffd046160560ff93ef4e3f1605978
sadat <azade.s.it@gmail.com>
2021-03-07 23:19:23 +1100

    feat: theme develpment

---
+++ src/theme/index.ts
@@ -0,0 +1,39 @@
+import { createMuiTheme } from "@material-ui/core";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#3498db",
+      contrastText: "#FFFFFF",
+    },
+    secondary: {
+      main: "#627b8b",
+      contrastText: "#FFFFFF",
+    },
+    text: {
+      primary: "#000000",
+      secondary: "#2c3e50",
+    //  cardLabel: "#34495e",
+    //  sectionTitle: "#b0b8bc",
+    },
+    background: {
+     // cardTitle: "#2c3e50",
+    },
+  },
+  typography:{
+    allVariants: {fontFamily: 'Merriweather' },
+    h1 : {
+      fontSize: 28,
+      fontFamily: 'MerriweatherSans',
+      fontWeight: 800,
+    },
+    subtitle2:{
+      fontSize: 10,
+    },
+    button:{
+      textTransform: "none",
+      fontSize: 16
+    }
+  }
+});
+export default theme;
